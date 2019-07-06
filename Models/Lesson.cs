ead7cb08028fbc039f944f3781ae8d165e01ed28
George Chaniotakis <haniotinho@gmail.com>
2019-07-06 20:57:29 +0300

    properties

--- Models/Lesson.cs
+++ Models/Lesson.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Web;
 
 namespace cloudrest.Models
 {
+    [DataContract]
     public class Lesson
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
     }
 }
--- Models/Student.cs
+++ Models/Student.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Web;
 
 namespace cloudrest.Models
 {
+    [DataContract]
     public class Student
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
     }
 }
