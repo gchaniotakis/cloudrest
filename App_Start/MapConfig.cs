ac8bfdf6f4cc4197e52b515f56fd11754f85eb13
George Chaniotakis <haniotinho@gmail.com>
2019-07-09 13:25:57 +0300

    jquery val + mapping

---
+++ App_Start/MapConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using cloudrest.Dtos;
+using cloudrest.Models;
+
+namespace cloudrest.App_Start
+{
+    public class MapConfig : Profile
+    {
+        public MapConfig()
+        {
+            Mapper.CreateMap<User, UserDto>();
+            Mapper.CreateMap<UserDto, User>()
+                .ForMember(m => m.UserId,opt => opt.Ignore());
+
+            Mapper.CreateMap<Lesson, LessonDto>();
+            Mapper.CreateMap<LessonDto, Lesson>()
+                .ForMember(m => m.LessonId, opt => opt.Ignore());
+
+            Mapper.CreateMap<Selection, SelectionDto>();
+            Mapper.CreateMap<SelectionDto, Selection>()
+                .ForMember(m => m.SelectionId, opt => opt.Ignore());
+
+            
+        }
+    }
+}
